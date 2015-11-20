80b5759172fe36534191208a4a59ec683549a681
mamir <magda.miranowicz@gmail.com>
2015-11-20 12:49:52 +0000

    Add localStorage

--- colour.js
+++ colour.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 $('button').on('click', function () {
 var color=$(this).attr('data-color');
-$('#page').css("background-color",color);
-localStorage.setItem('data-color', color);
+var background=$('#page').css("background-color",color);
+localStorage.setItem('#page', background);
 });
 });
 
